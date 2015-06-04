c4475adac7ed5c2c46e5c4210305a754dcf5d1f8
Jalandip Lepcha <jalandip@darjeeling.(none)>
2015-06-04 10:56:30 -0700

    Merge changes related to Command statistics

--- src/main/java/net/juniper/jmp/execution/JmpCommandResourceUnavailableException.java
+++ src/main/java/net/juniper/jmp/execution/JmpCommandResourceUnavailableException.java
@@ -1,5 +1,7 @@
 package net.juniper.jmp.execution;
 
 public class JmpCommandResourceUnavailableException extends Exception {
-
+  public JmpCommandResourceUnavailableException(String msg) {
+    super(msg);
+  }
 }
